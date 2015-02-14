1470519056672f73078917b4c7ae3074b3146668
sunny <indiasuny000@gmail.com>
2015-02-14 15:07:49 +0530

    Added ng-click on Open and Save buttons.

--- js/runCtrl.js
+++ js/runCtrl.js
@@ -1,3 +1,10 @@
 zestApp.controller('runCtrl', function ($scope) {
 
+  $scope.openFile = function () {
+    addon.port.emit('IMPORT');
+  }
+
+  $scope.saveFile = function () {
+    addon.port.emit('SAVE');
+  }
 });
